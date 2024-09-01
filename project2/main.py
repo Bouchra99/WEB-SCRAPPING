9463c9e3af69cb87dc35c397a5891b8a067f0a35
unknown <=>
2024-09-01 15:37:42 +0100

    add websites

--- project2/main.py
+++ project2/main.py
@@ -14,6 +14,7 @@ tables = soup.find_all("table", {"class" : "wikitable"})
 
 items = tables[2].tbody.find_all("tr")
 data = []
+count = 0
 for i in  range(1, len(items)) : 
     
     minestry = items[i].find_all("td")[1].text.strip()
@@ -22,9 +23,29 @@ for i in  range(1, len(items)) :
     
     party = items[i].find_all("td")[4].text.strip()
     details = ""
+    websites = []
+
     if items[i].find_all("td")[1].a is not None:
+
         details = base_url + items[i].find_all("td")[1].a["href"]
-        # print(website)
+
+        page2 = requests.get(details)
+        src2 = page2.content 
+        soup2 = BeautifulSoup(src2,"lxml")
+
+        infobox = soup2.find("table", {"class":"infobox"})
+
+        if infobox is not None : 
+            element = infobox.find_all("a", {"class": ["external free", "external text"]})
+            if len(element)>0 : 
+                for x in element: 
+                    print(x)
+                    if ".ma" in x["href"].strip():
+                        websites.append(x["href"])
+                        count+=1
+     
+
+        # print(infobox)
     else:
         print("no website")
 
@@ -34,12 +55,14 @@ for i in  range(1, len(items)) :
             "minestry" : minestry,
             "minester" : minester , 
             "party" : party, 
-            "details" : details
+            "details" : details, 
+            "websites" : websites
         }
     )
  
+print(len(data) , " and ", count)
 
-print(data)
+# print(data)
 
 df = pd.DataFrame(data)
 filename = 'project2/data.xlsx'
